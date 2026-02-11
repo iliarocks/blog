ffbf40aacc377be351b9cbc6e779b1bb21b787e3
iliarocks <parun.ilia@gmail.com>
2026-02-10 19:43:56 -0800

    chore: removed instant template todo app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,14 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
 export const metadata: Metadata = {
-  title: 'Create Instant App',
-  description: 'Instant DB Starter App',
+  title: {
+		default: 'Ilia',
+		template: '%s · Ilia',
+	},
+  description: 'Personal essays, projects, photos, and more',
 };
+
 
 export default function RootLayout({
   children,
@@ -24,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
